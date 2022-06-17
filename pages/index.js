2560e6e31dcc73dad502599c9a675ccb3f668240
Alan Gustavo Bardales Mendoza <alan@solotrucking.com>
2022-06-17 11:30:55 -0500

    Added meta data to page and display table

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
+import Head from "next/head"
 import Layout from "../components/layout"
 import {api} from '../utils/api'
+import dynamic from "next/dynamic"
+
+const EventsTable = dynamic(() => import('../components/eventsTable'), { ssr: false })
 
 export async function getStaticProps(){
   const res = await fetch(`${api}/events`)
@@ -12,11 +16,24 @@ export async function getStaticProps(){
   }
 }
 
+export const titlePage = "Bolxs!!";
+
 export default function Home({events}) {
   return (
     <>
+    <Head>
+      <link rel="icon" href="/favicon.ico" />
+      <meta name="og:title" content={titlePage} />
+      <meta
+        name="description"
+        content="Platoforma para la creacion y compra de tickets para eventos de todo tipo"
+      />
+      <meta name="twitter:card" content="summary_large_image" />
+      <title>{titlePage}</title>
+    </Head>
       <Layout>
         <div>Home</div>
+        <EventsTable events={events} />
       </Layout>
     </>
   )
