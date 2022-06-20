e4343b492c790a17478fa50f8642fcf5b4fe6c9d
Alan Gustavo Bardales Mendoza <alan@solotrucking.com>
2022-06-20 11:18:18 -0500

    Simple event page details

---
+++ components/productDetails.js
@@ -0,0 +1,165 @@
+import {
+    Box,
+    Container,
+    Stack,
+    Text,
+    Image,
+    Flex,
+    VStack,
+    Button,
+    Heading,
+    SimpleGrid,
+    StackDivider,
+    useColorModeValue,
+    List,
+    ListItem,
+  } from '@chakra-ui/react';
+  import { MdLocalShipping } from 'react-icons/md';
+  
+  export default function ProductDetails({ event }) {
+    return (
+      <Container maxW={'7xl'}>
+        <SimpleGrid
+          columns={{ base: 1, lg: 2 }}
+          spacing={{ base: 8, md: 10 }}
+          py={{ base: 18, md: 24 }}>
+          <Flex>
+            <Image
+              rounded={'md'}
+              alt={'product image'}
+              src={event.banner}
+              fit={'cover'}
+              align={'center'}
+              w={'100%'}
+              h={{ base: '100%', sm: '400px', lg: '500px' }}
+            />
+          </Flex>
+          <Stack spacing={{ base: 6, md: 10 }}>
+            <Box as={'header'}>
+              <Heading
+                lineHeight={1.1}
+                fontWeight={600}
+                fontSize={{ base: '2xl', sm: '4xl', lg: '5xl' }}>
+                {event.name}
+              </Heading>
+              <Text
+                color={useColorModeValue('gray.900', 'gray.400')}
+                fontWeight={300}
+                fontSize={'2xl'}>
+                {`${event.user.name} ${event.user.lastname}`}
+              </Text>
+            </Box>
+  
+            <Stack
+              spacing={{ base: 4, sm: 6 }}
+              direction={'column'}
+              divider={
+                <StackDivider
+                  borderColor={useColorModeValue('gray.200', 'gray.600')}
+                />
+              }>
+              <VStack spacing={{ base: 4, sm: 6 }}>
+                <Text
+                  color={useColorModeValue('gray.500', 'gray.400')}
+                  fontSize={'2xl'}
+                  fontWeight={'300'}>
+                  {event.description}
+                </Text>
+              </VStack>
+              <Box>
+                <Text
+                  fontSize={{ base: '16px', lg: '18px' }}
+                  color={useColorModeValue('yellow.500', 'yellow.300')}
+                  fontWeight={'500'}
+                  textTransform={'uppercase'}
+                  mb={'4'}>
+                  Lugar y fecha
+                </Text>
+  
+                <List spacing={2}>
+                  <ListItem>
+                    <Text as={'span'} fontWeight={'bold'}>
+                      Fecha:
+                    </Text>{' '}
+                    {event.start_date}
+                  </ListItem>
+                  <ListItem>
+                    <Text as={'span'} fontWeight={'bold'}>
+                      Lugar:
+                    </Text>{' '}
+                    {event.location_name}
+                  </ListItem>
+                  <ListItem>
+                    <Text as={'span'} fontWeight={'bold'}>
+                      Dirección:
+                    </Text>{' '}
+                    {event.location_address}
+                  </ListItem>
+                </List>
+              </Box>
+              <Box>
+                <Text
+                  fontSize={{ base: '16px', lg: '18px' }}
+                  color={useColorModeValue('yellow.500', 'yellow.300')}
+                  fontWeight={'500'}
+                  textTransform={'uppercase'}
+                  mb={'4'}>
+                  Boletos
+                </Text>
+  
+                <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+                    {event.ticket_types.length > 0 ? (
+                        event.ticket_types.map((type) => (
+                            <List key={type.ID} spacing={2}>
+                                <ListItem>
+                                    <Text as={'span'} fontWeight={'bold'}>
+                                        Nombre: 
+                                    </Text>{' '}
+                                    {type.name}
+                                </ListItem>
+                                <ListItem>
+                                    <Text as={'span'} fontWeight={'bold'}>
+                                        Precio: 
+                                    </Text>{' '}
+                                    {type.price}
+                                </ListItem>
+                                <ListItem>
+                                    <Text as={'span'} fontWeight={'bold'}>
+                                        Disponibles: 
+                                    </Text>{' '}
+                                    {type.available}
+                                </ListItem>
+                            </List>
+                        ))
+                    ) : (
+                        <Text>Evento gratuito</Text>
+                    )}
+                </SimpleGrid>
+              </Box>
+            </Stack>
+  
+            <Button
+              rounded={'none'}
+              w={'full'}
+              mt={8}
+              size={'lg'}
+              py={'7'}
+              bg={useColorModeValue('gray.900', 'gray.50')}
+              color={useColorModeValue('white', 'gray.900')}
+              textTransform={'uppercase'}
+              _hover={{
+                transform: 'translateY(2px)',
+                boxShadow: 'lg',
+              }}>
+              Add to cart
+            </Button>
+  
+            <Stack direction="row" alignItems="center" justifyContent={'center'}>
+              <MdLocalShipping />
+              <Text>2-3 business days delivery</Text>
+            </Stack>
+          </Stack>
+        </SimpleGrid>
+      </Container>
+    );
+  }
