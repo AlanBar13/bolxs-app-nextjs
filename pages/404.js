54595c1246d4b7303e18fe9eaaa1a46ee7157d20
Alan Gustavo Bardales Mendoza <alan@solotrucking.com>
2022-06-20 10:41:57 -0500

    Added simple 404 page

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Layout from "../components/layout"
+
+export default function NotFound() {
+    return (
+        <>
+            <Layout>
+                <h1>Error: 404</h1>
+                <p>Evento No Encontrado</p>
+            </Layout>
+        </>
+    )
+}
