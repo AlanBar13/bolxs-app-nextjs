722106230ca948fe82e459e668ac23bf64aa661f
Alan Gustavo Bardales Mendoza <alan@solotrucking.com>
2022-06-21 11:53:43 -0500

    Added redirect to create events

--- components/layout.js
+++ components/layout.js
@@ -1,5 +1,6 @@
-import Head from "next/head";
 import NLink from "next/link";
+import { useRouter } from 'next/router';
+import { useAuth } from "../context/AuthUserContext";
 import {
     Box,
     Flex,
@@ -38,6 +39,8 @@ const NavLink = ({ children, route }) => (
 
 export default function Layout({ children }) {
     const { isOpen, onOpen, onClose } = useDisclosure();
+    const { authUser, signOut } = useAuth();
+    const router = useRouter();
     return (
         <>
             <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
@@ -66,30 +69,37 @@ export default function Layout({ children }) {
                     colorScheme={'teal'}
                     size={'sm'}
                     mr={4}
+                    onClick={() => router.push('/createEvent')}
                     leftIcon={<AddIcon />}>
-                    Action
+                    Agregar Evento
                     </Button>
-                    <Menu>
-                    <MenuButton
-                        as={Button}
-                        rounded={'full'}
-                        variant={'link'}
-                        cursor={'pointer'}
-                        minW={0}>
-                        <Avatar
-                        size={'sm'}
-                        src={
-                            'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-                        }
-                        />
-                    </MenuButton>
-                    <MenuList>
-                        <MenuItem>Link 1</MenuItem>
-                        <MenuItem>Link 2</MenuItem>
-                        <MenuDivider />
-                        <MenuItem>Link 3</MenuItem>
-                    </MenuList>
-                    </Menu>
+                    {authUser ? (
+                        <Menu>
+                            <MenuButton
+                                as={Button}
+                                rounded={'full'}
+                                variant={'link'}
+                                cursor={'pointer'}
+                                minW={0}>
+                                <Avatar
+                                size={'sm'}
+                                src={
+                                    'https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
+                                }
+                                />
+                            </MenuButton>
+                            <MenuList>
+                                <MenuItem>Link 1</MenuItem>
+                                <MenuItem>Link 2</MenuItem>
+                                <MenuDivider />
+                                <MenuItem onClick={signOut}>Salir</MenuItem>
+                            </MenuList>
+                        </Menu>
+                    ) :(
+                        <Link href='/login'>
+                            Acceder
+                        </Link>
+                    )}
                 </Flex>
                 </Flex>
 
